7d8f9b3ff5f3c286cf5ebbcfe347babf908d1856
Jishnu2608 <jishnudeepborah@gmail.com>
2023-02-22 22:23:41 +0530

    52:00

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,9 +39,11 @@ const upload = multer({ storage });
 /*ROUTES WITH FILES*/
 app.post("/auth/register",upload.single("picture"),register);
 
+/*ROUTES*/
 
+app.use("/auth", authRoutes);
 
-/*mongoose setup*/
+/*MONGOOSE SETUP*/
 
 const PORT = process.env.PORT || 6001;
 mongoose
