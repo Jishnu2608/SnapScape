4b6bd762b26c31fb135244386bae8c5bd9d1069d
Jishnu2608 <jishnudeepborah@gmail.com>
2023-02-26 02:43:39 +0530

    info changed

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -24,12 +24,11 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>Canon EOS R6 Full-Frame Mirrorless Camera</Typography>
+        <Typography color={medium}>canon.com</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+      For the everyday shooter with high demands, the Canon EOS R6 is a versatile tool to meet the photo and video requirements of a contemporary imaging workflow. This full-frame mirrorless camera revolves around a refined 20MP CMOS sensor and DIGIC X processor, which afford quick shooting up to 12 fps, apt 4K60 video recording, and wide-ranging sensitivity to ISO 102400. It also sports 5-axis in-body image stabilization to help steady shots when working in low-light conditions.
       </Typography>
     </WidgetWrapper>
   );
